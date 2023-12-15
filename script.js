a008f3f364c3f8736da2ffb612e0ee4511da4803
njonusiene <jonusiene.n@gmail.com>
2023-12-15 20:39:11 +0200

    JS20

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-// Sukurkite papildomą puslapį, add.html, kuriame būtų forma su dviem inputais - brand ir model; šie paduotų su post'u informaciją į base url (formatas: objektas su dviem properties: brand ir model).
-// Sukurkite notification - t.y. sėkmingai užpildžius formą ir gavus patvirtinimą, turi virš formos rašyti, kad duomenys sėkmingai išsaugoti; o jei blogai - išmesti errorą.
-// Sukurkite navigaciją, kad galėtumėte tarp puslapių vaikščioti ir patikrinkite ar įrašyti duomenys atsivaizduoja :)
-
 const API_URL = "https://open-long-puck.glitch.me/"
 const form = document.getElementById("form")
 const carInput = document.getElementById("car")
 const modelInput = document.getElementById("model")
+const notification = document.getElementById("notification")
+
+const showNotification = (message, isSuccess) => {
+    notification.textContent = message
+    notification.classList.toggle("success", isSuccess)
+    notification.classList.toggle("error", !isSuccess)
+
+    setTimeout(() => {
+        notification.textContent = ""
+        notification.classList.remove("success", "error")
+    }, 4000)
+}
 
 const submitData = (e) => {
     e.preventDefault()
 
     if (!carInput.value || !modelInput.value) {
-        alert("Užpildykite visus laukelius")
+        showNotification("Užpildykite visus laukelius, prašau. :)", false)
         return
     }
 
     const newCar = {
         brand: carInput.value,
         model: modelInput.value,
-    }
+    };
 
     fetch(API_URL, {
         method: "POST",
@@ -34,12 +42,12 @@ const submitData = (e) => {
         carInput.value = ""
         modelInput.value = ""
 
-        alert("Duomenys sėkmingai išsaugoti")
+        showNotification("Duomenys išsaugoti! :)", true)
     })
     .catch((error) => {
         console.error("Klaida:", error)
-        alert("Įvyko klaida. Duomenys neišsaugoti.")
-    })
-}
+        showNotification("Įvyko klaida. Duomenys neišsaugoti. :(", false)
+    });
+};
 
 form.addEventListener("submit", submitData)
